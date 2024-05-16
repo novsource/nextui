cb50bd9eeeac5709a44a9729fd49542dc56f99f3
Poli Sour <polisour.work@gmail.com>
2024-05-16 15:47:24 +0800

    chore(app): init test app

--- apps/testing/tailwind.config.js
+++ apps/testing/tailwind.config.js
@@ -1,12 +1,16 @@
+const {nextui} = require("@nextui-org/theme/plugin")
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui({defaultTheme:'light'})],
 }
 
